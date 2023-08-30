8308ce247012c6437096350db0843f4a8d624d9f
alishakor <aliuokeowo@gmail.com>
2023-08-30 17:51:25 +0100

    force locale with UL parameter

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Flask app and babel class"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+# create the flask Instance
+app = Flask(__name__)
+
+
+class Config:
+    """Config class for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale() -> str:
+    """a function that best matches the language"""
+    locale = request.args.get("locale")
+    # check if the local query parameter is present in request
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+# create route decorator
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """returns the index.html page"""
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
