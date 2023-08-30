e0e4bfb15db2d376cd5e3ab93c6edcd510075490
alishakor <aliuokeowo@gmail.com>
2023-08-30 18:13:28 +0100

    fixing label localeselector decorator

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -17,12 +17,13 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale() -> str:
     """a function that best matches the language"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
 
 
 # create route decorator
